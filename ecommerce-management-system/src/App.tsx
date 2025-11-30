72863d5bde3d7e06bcace1ca90e3bec47fa183e7
Steven Zhao <stevenzhao0819@gmail.com>
2025-11-29 23:03:16 -0500

    Initial commit with simple todo app

---
+++ ecommerce-management-system/src/App.tsx
@@ -0,0 +1,39 @@
+import { use, useState } from 'react'
+import './App.css'
+
+// component
+function App() {
+  const [todos, setTodos] = useState<{title: string, id: number}[]>([])
+  const [text, setText] = useState('')
+
+
+  const handleAdd = () => {
+    console.log('text', text)
+    const newTodo = {
+      title: text,
+      id: new Date().valueOf(),
+    };
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+    setText('')
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <>
+       <input type="text" id="input" value={text} onChange={handleChange} />
+       <button onClick={handleAdd}>add</button>
+       {todos.length}
+       <ul>
+        {todos.map((todo)=> {
+          return <li key={todo.id}>{todo.title}</li>
+        })}
+       </ul>
+    </>
+  )
+}
+
+export default App

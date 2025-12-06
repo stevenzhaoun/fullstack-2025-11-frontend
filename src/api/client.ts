a12438ade73b3683a928f3be53d87b1e404211a7
Steven Zhao <stevenzhao0819@gmail.com>
2025-12-05 22:47:03 -0500

    update baseURL

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://fullstack-2025-11-backend.onrender.com',
 });
 
 export default instance
